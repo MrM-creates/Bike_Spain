f65f13a6f9b4c2cb3dd0d57caaa254b0ec39c2b1
MrM-creates <maniwinkler@icloud.com>
2026-09-26 08:28:56 +0200

    Update booking status: trip_adria_2026 / ljubljana-ring

--- data/trip-adria-2026.js
+++ data/trip-adria-2026.js
@@ -1,5 +1,5 @@
 globalThis.__TRIP_ADRIA_DATA__ = Object.freeze({
-  "publishedVersion": "2026-09-25T18:04:21.227Z",
+  "publishedVersion": "2026-09-26T06:28:55.921Z",
   "planKind": "adjusted",
   "originalDays": [
     {
@@ -1427,7 +1427,7 @@ globalThis.__TRIP_ADRIA_DATA__ = Object.freeze({
       "title": "Ljubljana",
       "startDate": "2026-09-27",
       "endDate": "2026-09-28",
-      "booking": "open",
+      "booking": "booked",
       "currentFirstChoice": "Urban Ring Hotel · Ljubljana-Rudnik",
       "currentFirstChoiceUrl": "https://www.booking.com/hotel/si/urban-ring.de.html?checkin=2026-09-27&checkout=2026-09-28&group_adults=2&no_rooms=1&group_children=0",
       "currentAlternative": "B&B Pod vrbo · Ljubljana-Trnovo",
@@ -1439,7 +1439,7 @@ globalThis.__TRIP_ADRIA_DATA__ = Object.freeze({
           "name": "Urban Ring Hotel · Ljubljana-Rudnik",
           "url": "https://www.booking.com/hotel/si/urban-ring.de.html?checkin=2026-09-27&checkout=2026-09-28&group_adults=2&no_rooms=1&group_children=0",
           "note": "",
-          "booking": "open",
+          "booking": "booked",
           "coordinate": [
             14.54101,
             46.021107
@@ -2146,7 +2146,7 @@ globalThis.__TRIP_ADRIA_DATA__ = Object.freeze({
   "trip": {
     "id": "trip_adria_2026",
     "name": "Adria & Balkan 2026",
-    "dataVersion": "2026-09-25T18:04:21.227Z",
+    "dataVersion": "2026-09-26T06:28:55.921Z",
     "characterTitle": "Adriawind, Küstenkurven und stille Buchten",
     "characterText": "Über Österreich und Slowenien an die dalmatinische Küste, drei Nächte an der Bucht von Kotor und zwei Nächte in Shkodër. Von Durrës führt die Nachtfähre nach Ancona; direkt weiter nach Urbino und auf der bestehenden Route durch Norditalien heim.",
     "startDate": "2026-09-24",
